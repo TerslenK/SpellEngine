56a3784798ac1786e80fa956b03f0116485e5cea
TerslenK <88115382+TerslenK@users.noreply.github.com>
2026-05-16 22:50:49 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+group = "com.example" // TODO: Change this to your group
+version = "1.0-SNAPSHOT" // TODO: Change this to your addon version
+
+plugins {
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.nova)
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.xenondevs.xyz/releases/")
+}
+
+dependencies {
+    implementation(libs.nova)
+}
+
+addon {
+    name = project.name.replaceFirstChar(Char::uppercase)
+    version = project.version.toString()
+    main = "com.example.ExampleAddon" // TODO: Change this to your main class
+    
+    // output directory for the generated addon jar is read from the "outDir" project property (-PoutDir="...")
+    val outDir = project.findProperty("outDir")
+    if (outDir is String)
+        destination = File(outDir)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+}
